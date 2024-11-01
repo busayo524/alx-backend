669eb62e4c49efc67d77a572d5e863ccec509225
busayo524 <olowookerebusayo66@gmail.com>
2024-11-01 23:50:51 +0000

    Implement LIFOCache with LIFO eviction policy

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+LIFOCache
+"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    LIFOCache
+    """
+
+    def __init__(self):
+        """
+        Initializer
+        """
+        super().__init__()
+        self.hold = []
+
+    def put(self, key, item):
+        """
+        put for key items
+        Args:
+            key(str): key of dictionary
+            item(Any): value of dictionary
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last_item = self.hold.pop()
+                del self.cache_data[last_item]
+                print(f"DISCARD: {last_item}")
+            self.cache_data[key] = item
+            self.hold.append(key)
+
+    def get(self, key):
+        """
+        get key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
