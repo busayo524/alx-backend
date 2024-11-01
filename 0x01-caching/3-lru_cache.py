1160788ed08941791ef40dc25c7f8826b16ae76f
busayo524 <olowookerebusayo66@gmail.com>
2024-11-01 23:56:07 +0000

    Introduce LRUCache with least-recently-used eviction

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+LIFOCache
+"""
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    LIFOCache
+    """
+
+    def __init__(self):
+        """
+        Initializer
+        """
+        super().__init__()
+        self.hold = []
+
+    def put(self, key, item):
+        """
+        put for key items
+        Args:
+            key(str): key of dictionary
+            item(Any): value of dictionary
+        """
+        if key in self.cache_data:
+            self.hold.remove(key)
+
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lfu = self.hold.pop(0)
+                del self.cache_data[lfu]
+                print(f"DISCARD: {lfu}")
+            self.cache_data[key] = item
+            self.hold.append(key)
+
+    def get(self, key):
+        """
+        get key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.hold.remove(key)
+        self.hold.append(key)
+        return self.cache_data[key]
